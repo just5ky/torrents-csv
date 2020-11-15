4f99a8a1be082298c17d490936f1aaa82dfe20c7
Heretic <tylerjobsearch06@gmx.com>
2020-11-15 14:59:00 -0600

    Adding prod dockerfile.

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -6,7 +6,7 @@ new_tag="$1"
 git tag $new_tag
 
 # Changing the docker-compose prod
-sed -i "s/dessalines\/torrents-csv:.*/dessalines\/torrents-csv:$new_tag/" ../prod/docker-compose.yml
+sed -i "s/dessalines\/torrents-csv-server:.*/dessalines\/torrents-csv-server:$new_tag/" ../prod/docker-compose.yml
 git add ../prod/docker-compose.yml
 
 # The commit
@@ -16,9 +16,9 @@ git commit -m"Upping version."
 # git push
 
 # Rebuilding docker
-sudo docker build ../../ --file ../dev/Dockerfile -t torrents-csv:latest
-sudo docker tag torrents-csv:latest dessalines/torrents-csv:$new_tag
-sudo docker push dessalines/torrents-csv:$new_tag
+sudo docker build ../../ --file ../prod/Dockerfile -t torrents-csv-server:latest
+sudo docker tag torrents-csv-server:latest dessalines/torrents-csv-server:$new_tag
+sudo docker push dessalines/torrents-csv-server:$new_tag
 
 # SSH and pull it
 # ssh tyler@45.55.175.59 "cd ~/git/torrents.csv/scripts/ && ./git_update.sh && cd ../docker/prod && git pull && docker-compose up -d"
